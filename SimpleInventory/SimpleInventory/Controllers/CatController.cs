7c93705812ea9030c4c24adf4e996c91aa2da062
TheodolosChristou <theodoulos.christou@gmail.com>
2025-09-21 18:50:13 +0300

    get and post pages for categories

---
+++ SimpleInventory/SimpleInventory/Controllers/CatController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleInventory.Domain.DTOs;
+using SimpleInventory.Domain.Models;
+using SimpleInventory.Domain.Services;
+
+namespace SimpleInventory.Controllers
+{
+    [Route("Category")]     // URLs: /Category, /Category/Create
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        // GET /Category
+        [HttpGet("")]
+        public async Task<IActionResult> Index()
+        {
+            var categories = _categoryService.getAllCategories();
+            return View(categories);  // Views/Category/Index.cshtml
+        }
+
+        // GET /Category/Create
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View(new Category());
+        }
+
+        // POST /Category/Create
+        [HttpPost("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            await _categoryService.createCategory(model);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
